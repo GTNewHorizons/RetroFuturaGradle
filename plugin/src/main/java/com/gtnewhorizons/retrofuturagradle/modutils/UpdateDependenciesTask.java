6df87c5ceff614ce9a0f9b63f832bef5b0457ea1
miozune <miozune@gmail.com>
2023-05-18 01:35:25 +0900

    Continue processing dependencies if one gets stuck (#36)

--- plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/modutils/UpdateDependenciesTask.java
+++ plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/modutils/UpdateDependenciesTask.java
@@ -53,6 +53,10 @@ public class UpdateDependenciesTask extends DefaultTask {
             if (modName == null || currentVersion == null) continue;
 
             List<String> versions = fetchVersions(modName);
+            if (versions == null) {
+                // Something went wrong while fetch. Assume logging is already done
+                continue;
+            }
             if (versions.isEmpty()) {
                 getLogger().warn(String.format("No releases found on %s", modName));
                 continue;
@@ -92,19 +96,21 @@ public class UpdateDependenciesTask extends DefaultTask {
     private List<String> fetchVersions(String modName) {
         // Currently works only with GTNH repositories
         URL url;
+        String urlString = String.format(
+                "http://jenkins.usrv.eu:8081/nexus/content/groups/public/com/github/GTNewHorizons/%s/maven-metadata.xml",
+                modName);
         try {
-            url = new URL(
-                    String.format(
-                            "http://jenkins.usrv.eu:8081/nexus/content/groups/public/com/github/GTNewHorizons/%s/maven-metadata.xml",
-                            modName));
+            url = new URL(urlString);
         } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            getLogger().error("Error generating URL: {}", urlString, e);
+            return null;
         }
         URLConnection connection;
         try {
             connection = url.openConnection();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            getLogger().error("Failed to establish connection: {}", urlString, e);
+            return null;
         }
 
         Document document;
@@ -112,7 +118,8 @@ public class UpdateDependenciesTask extends DefaultTask {
             DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             document = builder.parse(connection.getInputStream());
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            getLogger().error("Could not fetch version: {}", urlString, e);
+            return null;
         }
         NodeList versionElements = document.getElementsByTagName("version");
         List<String> versions = new ArrayList<>();
