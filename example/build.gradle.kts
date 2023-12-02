751839daa9bf2d54a5554b3c10f31d33917ba761
Raven Szewczyk <git@eigenraven.me>
2023-12-02 15:11:34 +0000

    Update versions in the example mod

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   id("maven-publish")
   id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
   id("eclipse")
-  id("com.gtnewhorizons.retrofuturagradle") version "1.3.19"
+  id("com.gtnewhorizons.retrofuturagradle") version "1.3.25"
 }
 
 // Project properties
