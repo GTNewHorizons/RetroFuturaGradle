525eeebb7681f6a59d5e51ffc5d47c6d647b6a5a
makamys <makamys@outlook.com>
2024-02-11 13:35:01 +0100

    Actually remap sources

--- plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/modutils/MigrateMappingsTask.java
+++ plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/modutils/MigrateMappingsTask.java
@@ -1,16 +1,23 @@
 package com.gtnewhorizons.retrofuturagradle.modutils;
 
 import com.gtnewhorizons.retrofuturagradle.mcp.GenSrgMappingsTask;
+import com.gtnewhorizons.retrofuturagradle.mcp.InjectTagsTask;
+import com.gtnewhorizons.retrofuturagradle.mcp.SharedMCPTasks;
 import com.gtnewhorizons.retrofuturagradle.util.Utilities;
 import com.opencsv.CSVReader;
+import org.apache.commons.io.FileUtils;
 import org.cadixdev.lorenz.MappingSet;
 import org.cadixdev.lorenz.io.MappingFormats;
 import org.cadixdev.lorenz.io.MappingsWriter;
 import org.cadixdev.lorenz.model.FieldMapping;
 import org.cadixdev.lorenz.model.MethodMapping;
 import org.cadixdev.lorenz.model.TopLevelClassMapping;
+import org.cadixdev.mercury.Mercury;
+import org.cadixdev.mercury.mixin.MixinRemapper;
+import org.cadixdev.mercury.remapper.MercuryRemapper;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.jvm.tasks.Jar;
 import org.gradle.api.tasks.options.Option;
 
 import java.io.File;
@@ -22,23 +29,41 @@ public class MigrateMappingsTask extends DefaultTask {
 
     private static final boolean DEBUG_WRITE_DIFF = Boolean.parseBoolean(System.getenv("RFG_DEBUG_WRITE_MAPPING_DIFF"));
 
-    private String mcpDir;
+    private File mcpDir;
+    private File inputDir;
+    private File outputDir;
 
-    @Option(option = "mcpDir", description = "A directory containing the mappings to migrate to, using MCP's fields.csv and methods.csv format.")
+    @Option(option = "mcpDir", description = "The directory containing the mappings to migrate to, using MCP's fields.csv and methods.csv format.")
     public void setMcpDir(String mcpDir) {
-        this.mcpDir = mcpDir;
+        this.mcpDir = new File(mcpDir);
+    }
+
+    @Option(option = "inputDir", description = "The directory containing the source code to migrate.")
+    public void setInputDir(String inputDir) {
+        this.inputDir = new File(inputDir);
+    }
+
+    @Option(option = "outputDir", description = "The directory the migrated source code should be written to.")
+    public void setOutputDir(String outputDir) {
+        this.outputDir = new File(outputDir);
     }
     
     @TaskAction
-    public void migrateMappings() throws IOException {
+    public void migrateMappings() throws Exception {
         if(mcpDir == null) {
             throw new IllegalArgumentException("--mcpDir must be provided.");
+        }
+        if(inputDir == null) {
+            inputDir = new File(getProject().getProjectDir(), "src/main/java");
+        }
+        if(outputDir == null) {
+            outputDir = new File(getProject().getProjectDir(), "src/main/java");
         }
         GenSrgMappingsTask genSrgMappings = (GenSrgMappingsTask)getProject().getTasks().getByName("generateForgeSrgMappings");
         File currentFields = genSrgMappings.getFieldsCsv().getAsFile().get();
         File currentMethods = genSrgMappings.getMethodsCsv().getAsFile().get();
 
-        File target = new File(mcpDir);
+        File target = mcpDir;
         File srg = genSrgMappings.getInputSrg().getAsFile().get();
 
         MappingSet notchSrg = MappingFormats.SRG.read(srg.toPath());
@@ -51,6 +76,25 @@ public class MigrateMappingsTask extends DefaultTask {
                 w.write(diffMcp);
             }
         }
+
+        final Mercury mercury = new Mercury();
+
+        // There's probably a better way to do this
+        mercury.getClassPath().add(((Jar)getProject().getTasks().getByName("packagePatchedMc")).getArchiveFile().get().getAsFile().toPath());
+        mercury.getClassPath().add(((InjectTagsTask)getProject().getTasks().getByName("injectTags")).getOutputDir().getAsFile().get().toPath());
+        for (File file : getProject().getConfigurations().getByName("compileClasspath").getFiles()) {
+            mercury.getClassPath().add(file.toPath());
+        }
+        // Fixes some issues like broken javadoc in Forge, and JDT not understanding Hodgepodge's obfuscated voxelmap targets.
+        mercury.setGracefulClasspathChecks(true);
+
+        mercury.getProcessors().add(MixinRemapper.create(diffMcp));
+        mercury.getProcessors().add(MercuryRemapper.create(diffMcp));
+
+        // TODO actually get this from the project
+        mercury.setSourceCompatibility("17");
+
+        mercury.rewrite(inputDir.toPath(), outputDir.toPath());
     }
 
     /** Returns the difference between two mappings of the same root.
