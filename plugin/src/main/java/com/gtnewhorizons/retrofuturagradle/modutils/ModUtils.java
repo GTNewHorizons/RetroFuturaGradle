62ac0ae71df05cc06fc3309e12ea8e864f68bd63
makamys <makamys@outlook.com>
2024-02-11 19:36:20 +0100

    Set task dependencies

--- plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/modutils/ModUtils.java
+++ plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/modutils/ModUtils.java
@@ -109,6 +109,10 @@ public class ModUtils {
             task.setGroup(TASK_GROUP_USER);
             task.setDescription(
                     "Migrate main source set to a new set of mappings");
+            task.dependsOn(project.getTasks().getByName("taskExtractMcpData"));
+            task.dependsOn(project.getTasks().getByName("taskExtractForgeUserdev"));
+            task.dependsOn(project.getTasks().getByName("packagePatchedMc"));
+            task.dependsOn(project.getTasks().getByName("injectTags"));
         });
 
         if (!disableDependencyDeobfuscation) {
