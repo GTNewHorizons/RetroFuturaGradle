0a23d6c9f17806f4888513f8720fdd623a077dfc
Raven Szewczyk <git@eigenraven.me>
2024-12-07 21:32:29 +0000

    Add a deprecation notice for running Gradle under Java 8-20. Modding for Java 8 WILL still be supported, this is just about Gradle itself.

--- plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/UserDevPlugin.java
+++ plugin/src/main/java/com/gtnewhorizons/retrofuturagradle/UserDevPlugin.java
@@ -3,6 +3,7 @@
  */
 package com.gtnewhorizons.retrofuturagradle;
 
+import org.gradle.api.JavaVersion;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaLibraryPlugin;
@@ -23,6 +24,12 @@ public class UserDevPlugin implements Plugin<Project> {
 
         if (GradleVersion.current().compareTo(GradleVersion.version("7.6")) < 0) {
             throw new IllegalStateException("Using RetroFuturaGradle requires at least Gradle 7.6.");
+        }
+
+        if (!JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_21)) {
+            project.getLogger().error(
+                    "[DEPRECATION NOTICE] Support for running Gradle using Java versions older than 21 with the RetroFuturaGradle plugin is scheduled to be removed, please upgrade your local and CI workflow Java version to 21 or newer. "
+                            + "This does NOT mean that mod code has to use newer Java, the Gradle process itself will require Java 21 but the compiler and mod code can keep on using Java 8.");
         }
 
         RfgCacheService.register(project.getGradle());
